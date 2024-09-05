943a48a4563e3fb3c131c2caa4e7f1d81baf2619
Si <tlgyrjs@naver.com>
2024-09-05 17:19:31 +0900

    d

---
+++ baekJoon/bronze/bronze_3/B_15894.java
@@ -0,0 +1,12 @@
+package baekJoon.bronze.bronze_3;
+
+
+import java.util.Scanner;
+
+public class B_15894 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        System.out.println(a*4);
+    }
+}
